11e30e5137260e59e624a891f76b26e26dff00ce
Saeed Tehrani <saeed.tehrani@gmail.com>
2020-08-31 09:27:34 -0400

    Improved user interface to accomodate API configuration

--- js/plot.js
+++ js/plot.js
@@ -10,8 +10,8 @@ var config = {
   }
 };
 	let url='https://api.mockaroo.com/api/0c0b0d20?key=92ae5410&?count=20&?array=true';    
-    $.get(url).done(function (data) {    	
-    	//console.log(data);
+    /*$.get(url).done(function (data) {    	
+    	console.log(data);
     	const xdata = data.map((_,i)=>data[i].x);
   		const ydata = data.map((_,i)=>data[i].y);
   		let sortxdata = xdata.sort(function(a, b){return a - b});
@@ -30,8 +30,28 @@ var config = {
                   'y' : predydata,
                   'name' : 'Regression Line',
                   'mode' : 'lines'}];
-    	Plotly.newPlot('div1',[pdata[0],pdata[1]],{legend:{x:1,xanchor:'right',y:1.1,orientation:'h',font:{size:20}},title:{text:'Data From Mockaroo',font:{size:32}},xaxis:{title:{text:'X',font:{size:24}},dtick:1},yaxis:{title:{text:'Y',font:{size:24}}}},config);
+    	Plotly.newPlot('div1',[pdata[0],pdata[1]],{legend:{x:1,xanchor:'right',y:1.1,orientation:'h',font:{size:20}},xaxis:{title:{text:'X',font:{size:24}},dtick:1},yaxis:{title:{text:'Y',font:{size:24}}}},config);
     	$("#div2").append('Line Equation'+regression.toString());
-	});
+	});*/
+
+    const xdata = data.map((_,i)=>data[i].x);
+    const ydata = data.map((_,i)=>data[i].y);
+    let sortxdata = xdata.sort(function(a, b){return a - b});
+    sortxdata = sortxdata.map((_,i)=>parseFloat(sortxdata[i]));
+    let predydata = [];
+    const regression = new ML.SimpleLinearRegression(xdata,ydata);
+    for (let i=0; i<sortxdata.length; i++){
+      predydata[i] = Math.round(regression.predict(sortxdata[i]));//Math.round((regression.predict(sortxdata[i])*100)+Number.EPSILON);
+    }            
+    const pdata = [{'x' : xdata,                  
+                  'y' : ydata,
+                  'name' : 'Actual Data',
+                  'mode' : 'markers'},
+                  {'x' : sortxdata,                  
+                  'y' : predydata,
+                  'name' : 'Regression Line',
+                  'mode' : 'lines'}];
+    Plotly.newPlot('div1',[pdata[0],pdata[1]],{legend:{x:1,xanchor:'right',y:1.1,orientation:'h',font:{size:20}},xaxis:{title:{text:'X',font:{size:24}},dtick:10},yaxis:{title:{text:'Y',font:{size:24}}}},config);
+    $("#div2").append('Line Equation '+regression.toString());
 
 	});
